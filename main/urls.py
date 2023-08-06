c0216dbe23e42ae6122f1c19784ede3af2d2c26c
belemaaa <graceitamunoala@gmail.com>
2023-08-06 01:49:03 +0000

    fixes and ids

--- main/urls.py
+++ main/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name="index"),
     path('register/', views.register, name="register"),
     path('login/', views.login, name="login"),
-    path('', views.index, name="index"),
-    path('book appointment/', views.book_appointment, name='book_appointment'),
-    path('view apointments/', views.view_appointments, name='view-appointments'),
-    path('health data', views.input_health_data, name="health_data"),
+    path('book_appointment/', views.book_appointment, name='book_appointment'),
+    path('view_apointments/', views.view_appointments, name='view_appointments'),
+    path('health_data', views.input_health_data, name="health_data"),
     path('ambulance', views.request_ambulance, name="ambulance"),
 
 ]
